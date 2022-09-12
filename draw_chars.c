3a3e18a9c546a347192cbeffec4c1aa7a613ba47
eagarcia31 <eagarcia31@miners.utep.edu>
2022-09-12 16:24:08 -0600

    working on 8x12

--- draw_chars.c
+++ draw_chars.c
@@ -16,7 +16,7 @@ void print_char_11x16(char c)
 
 void print_char_5x7(char c)
 {
-  c -= 0x40;
+  c -= 0x20;
   for (char row = 0; row < 5; row++) {
     unsigned short rowBits = font_5x7[c][row];
     for (char col = 0; col < 7; col++) {
@@ -29,7 +29,7 @@ void print_char_5x7(char c)
 
 void print_char_8x12(char c)
 {
-  c -=0x40;
+  c -=0x20; /*Change along with shift*/
   for (char row = 0; row < 8; row++) {
     unsigned short rowBits = font_8x12[c][row];
     for (char col = 0; col < 12; col++) {
